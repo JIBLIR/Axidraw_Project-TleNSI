8010945db1ce3717615c512b46f41d6043844d98
JIBLIR <157957198+JIBLIR@users.noreply.github.com>
2025-12-16 14:16:21 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,11 +1,14 @@
+# importation des bibliothèques
+import requests 
 
-def imprimer_svg_client(svg):
+url = "http://172.16.100.30:5432/recevoir"
+
+def imprimer_svg_client(svg,url):
 	'''envoie sur le serveur le fichier svg pour imprimer'''
-	import requests 
 
-	url = "http://172.16.100.30:5432/recevoir"
-
+	# création du dictionnaire "chaine" contenant le svg
 	chaine = {'message' : svg}
+	# transfert du svg au serveur
 	response = requests.post(url, data = chaine)
-
+	# retourne la reponse
 	return response.text
