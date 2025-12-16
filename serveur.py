2c2f48b091e6a14c7dbb15b0d670dd7973e56fb4
JIBLIR <157957198+JIBLIR@users.noreply.github.com>
2025-12-16 14:09:23 +0100

    Update and rename serveur2.py to serveur.py

--- serveur.py
+++ serveur.py
@@ -1,39 +1,38 @@
-"""
-Source - https://stackoverflow.com/a
-Posted by Tola, modified by community. See post 'Timeline' for change history
-Retrieved 2025-12-02, License - CC BY-SA 4.0
-"""
 from pyaxidraw import axidraw   
 import cherrypy 
 import os
 
 def index():
+	''' instanciation du serveur web, de la page index '''
 	return "Hello"
-index.exposed = True
 
 def recevoir(message,imprimer=True):
-	#https://stackoverflow.com/questions/71104397/how-to-convert-svg-string-to-svg-file-using-python
+	# source pour le traitement du svg : https://stackoverflow.com/questions/71104397/how-to-convert-svg-string-to-svg-file-using-python
 	with open("svgTest.svg", "w") as svg_file:
 			svg_file.write(message)
 			print("fichier enregistré")
-			
+	# imprime le svg si imprimer = True
 	if imprimer:
 		ad = axidraw.AxiDraw()
 		ad.plot_setup("svgTest.svg")
 		ad.plot_run()
 	
 	print(message)
+	# retourne le svg reçu
 	return f"Chaine reçue : {message}"
-	
+
+# exposition des pages
+index.exposed = True
 recevoir.exposed = True
 
-
+# configuration réseau
 cherrypy.config.update({
         'server.socket_host': '0.0.0.0',   # Accessible depuis tout le réseau
         'server.socket_port': 5432,
         'server.thread_pool': 8,}
     )
 
+# lancement du serveur web
 cherrypy.tree.mount(index, "/")
 cherrypy.tree.mount(recevoir, "/recevoir")
 
